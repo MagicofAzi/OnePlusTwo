9ae7e4aaaa9c530bca079d5f835fe34acf8af0e7
Adeola Uthman <authman2@gmail.com>
2017-10-08 17:58:32 -0400

    You can now quit the app by clicking on it while holding the control key

--- OnePlusTwo_Project/main.js
+++ OnePlusTwo_Project/main.js
@@ -1,9 +1,17 @@
 var menubar = require('menubar');
+var Menu = require('electron').Menu;
+
 var mb = menubar({
 	width: 300,
-	height: 520
+	height: 520,
+	tooltip: "1+2"
 });
 
 mb.on('ready', function ready () {
-	
+	// When the tray is clicked and the control key is held down.
+	mb.tray.on('click', (e, bounds) => {
+		if(e.ctrlKey === true) {
+			mb.app.quit();
+		}
+	})
 });
